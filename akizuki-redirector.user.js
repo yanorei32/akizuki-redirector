03aab5de49935e4386e26cac7faf2631f2f42f4c
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2024-01-26 10:38:39 +0900

    Improve readability

--- akizuki-redirector.user.js
+++ akizuki-redirector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Akizuki Redirector
 // @namespace    https://github.com/yanorei32/akizuki-redirector
-// @version      0.1.2
+// @version      0.1.3
 // @description  新しいURLにリダイレクトします。たぶん。
 // @author       yanorei32
 // @match        https://akizukidenshi.com/catalog/g/*
@@ -14,9 +14,9 @@
 (function() {
     'use strict';
     const url_parts = location.href.split('/');
-    const old_part_id_candidates = url_parts.filter(v => v.includes('-'));
-    if (old_part_id_candidates.length != 1) return;
-    const old_part_id = old_part_id_candidates[0];
-    const numeric_part = old_part_id.split('-')[1];
+    const old_id_candidates = url_parts.filter(v => v.includes('-'));
+    if (old_id_candidates.length != 1) return;
+    const old_id = old_id_candidates[0];
+    const numeric_part = old_id.split('-')[1];
     location.href = `/catalog/g/g1${numeric_part}`;
 })();
